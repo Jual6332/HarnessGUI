35dee3701e2fb53c52a85bd4cc6d6ca7655e5d18
Justin <jual6332@colorado.edu>
2017-06-22 14:48:51 -0600

    Additional clean-up, error checking

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-from tkinter import messagebox
 import subprocess
+from tkinter import messagebox
 
 LARGE_FONT = ("Verdana", 18)
 
@@ -20,44 +20,36 @@ class Application(tk.Tk):
 	StatusPage_checks = {} # Checks to make, labels for Status Check page
 
 	def __init__(self,*args,**kwargs):
-		tk.Tk.__init__(self,*args,**kwargs)
-		tk.Tk.wm_title(self,"Harness Interface")
-		self.container = tk.Frame(self)
-		self.container.pack(side="top", fill="both", expand=True)
-		self.container.grid_rowconfigure(0,weight=1); self.container.grid_columnconfigure(0,weight=1)
-		self.frames = {}; self.gather_StatusPage_checks() # Fill Status Page dictionary
+		tk.Tk.__init__(self,*args,**kwargs); tk.Tk.wm_title(self,"Harness Interface");
+		self.container = tk.Frame(self); self.container.pack(side="top", fill="both", expand=True);
+		self.container.grid_rowconfigure(0,weight=1); self.container.grid_columnconfigure(0,weight=1);
+		self.frames = {}; self.gather_StatusPage_checks(); # Fill Status Page dictionary
 		for F in (StartPage, DetailsPage, EditConfigsPage, StatusCheckPage):
 			frame = F(self.container,self); self.frames[F] = frame; frame.grid(row=0, column=0, sticky="nsew") # Create, store, grid
 		self.show_frame(StartPage) # Show Home
 
 	def gather_StatusPage_checks(self):
-		for I in ["Configuration Settings Loaded","Configuration Settings Set","Harness Script Run"]:
-			Application.StatusPage_checks[I] = "PASS"
+		for I in ["Configuration Settings Loaded","Configuration Settings Set","Harness Script Run"]: Application.StatusPage_checks[I] = "PASS"
 
 	def show_frame(self, cont):
-		for frame in self.frames.values():
-			frame.grid_remove()
+		for frame in self.frames.values(): frame.grid_remove()
 		frame = self.frames[cont]
 		if cont.__name__ == "DetailsPage" and Application.labelsGone:
-			del frame
-			frame = DetailsPage(self.container,self)
+			del frame; frame = DetailsPage(self.container,self);
 			self.frames[DetailsPage] = frame
 		elif cont.__name__ == "StatusCheckPage": # Make for loop with these two pages being deleted, added
-			del frame
-			frame = StatusCheckPage(self.container,self)
+			del frame; frame = StatusCheckPage(self.container,self);
 			self.frames[StatusCheckPage] = frame
 		frame.grid(row=0, column=0, sticky="nsew")
 
 # Global Functions
-def close_app():
-	print("Closing window...Closed."); app.quit()
+def close_app(): print("Closing window...Closed."); app.quit();
 
-def initialize_class(self,parent,controller):
+def initialize_class(self,parent,controller): 
 	tk.Frame.__init__(self,parent); self.controller = controller; self.set_page(controller);
 
-def pad_children(self):
-	for child in self.winfo_children():
-		child.grid_configure(padx=5, pady=5)
+def pad_children(self): 
+	for child in self.winfo_children(): child.grid_configure(padx=5, pady=5);
 
 def runscript_callback(controller):
 	noerror = False; 
@@ -76,11 +68,11 @@ def runscript_callback(controller):
 def window_asktocancel(pageName,controller,errorQuery):
 	if not errorQuery:
 		if messagebox.askokcancel("Quit", "Harness is running. Are you sure you want to quit?"):
-			if pageName == "Home": controller.show_frame(StartPage)
-			elif pageName == "Quit": close_app()
+			if pageName == "Home": controller.show_frame(StartPage);
+			elif pageName == "Quit": close_app();
 	else:
-		if pageName == "Home": controller.show_frame(StartPage)
-		elif pageName == "Quit": close_app()
+		if pageName == "Home": controller.show_frame(StartPage);
+		elif pageName == "Quit": close_app();
 
 def window_popup(title,message):
    tk.messagebox.showinfo(title, message)
@@ -90,41 +82,32 @@ def window_callwait(message,self):
 
 def window_loadwait(message,self):
 	win = tk.Toplevel(self); win.transient(); win.title("");
-	label = tk.Label(win, font = "Helvetica 12", text=message)
-	label.grid(row=0, column=0,columnspan=3); label.grid_configure(padx=10, pady=10);
+	label = tk.Label(win, font = "Helvetica 12", text=message); label.grid(row=0, column=0,columnspan=3); label.grid_configure(padx=10, pady=10);
 	return win
 
 # Config Data Functions
 def load_configdata():
-	inputdata = {}
+	inputdata = {};
 	if (Application.firstEdit or not Application.changesMade): # Better way: Simplify to one with statement!!!!
 		with open('Logs/harness_log.txt') as f:
 			data = f.readlines()
 			for item in data:
-				splice = item.split(":")
-				no_newline = splice[1].split("\n")
-				no_space = no_newline[0].split(" ")
-				Application.configData[splice[0]] = no_space[1]
-				Application.count = Application.count+1
-				inputdata = Application.configData
-				Application.key_order.append(splice[0])
+				splice = item.split(":"); no_newline = splice[1].split("\n"); no_space = no_newline[0].split(" ");
+				Application.configData[splice[0]] = no_space[1]; Application.count = Application.count+1;
+				inputdata = Application.configData; Application.key_order.append(splice[0]);
 		write_configdata("Backup")
 	else: # If changes have been made, load output file
 		with open('Logs/out.txt') as f:
 			data = f.readlines()
 			for item in data:
-				splice = item.split(":")
-				no_newline = splice[1].split("\n")
-				no_space = no_newline[0].split(" ")
-				inputdata[splice[0]] = no_space[1]
+				splice = item.split(":"); no_newline = splice[1].split("\n");
+				no_space = no_newline[0].split(" "); inputdata[splice[0]] = no_space[1];
 	return inputdata
 
 def remove_configitems():
 	if (not Application.labelsGone):
-		for label in Application.DetailsPage_labels:
-			label.grid_remove(); label.destroy();
-		for entry in Application.DetailsPage_entries:
-			entry.grid_remove(); entry.destroy();
+		for label in Application.DetailsPage_labels: label.grid_remove(); label.destroy();
+		for entry in Application.DetailsPage_entries: entry.grid_remove(); entry.destroy();
 		Application.labelsGone = True
 
 def save_configdata(self,controller): # Gather Entry Data, if changed then Output new values
@@ -133,11 +116,12 @@ def save_configdata(self,controller): # Gather Entry Data, if changed then Outpu
 		Application.sortedData[key] = Application.EditPage_entries[num].get() # Each key changed, gets its new value
 		if key == "forecast" and (not Application.sortedData[key].isalpha()):
 			check = False; errorMsg = errorMsg + "- Item 'forecast' must contain only letters\n";
+		elif key == "forecast" and (Application.sortedData[key] not in {"no","yes"}):
+			check = False; errorMsg = errorMsg + "- Item '"+key+"' must be either yes or no\n";
 		elif key != "forecast" and (not Application.sortedData[key].isnumeric()):
 			check = False; errorMsg = errorMsg + "- Item '"+key+"' must contain only numbers\n";
 		else:
-			if Application.configData[key] != Application.sortedData[key]:
-				Application.changesMade = True
+			if Application.configData[key] != Application.sortedData[key]: Application.changesMade = True;
 		num = num+1
 	if check: # Save changes to CIs
 		if (Application.changesMade):
@@ -149,11 +133,9 @@ def save_configdata(self,controller): # Gather Entry Data, if changed then Outpu
 def write_configdata(method):
 	num = 1 # printing incrementally
 	if method == "Backup":
-		config = open('Logs/harness_log_backup.txt', 'w')
-		check_dict = Application.configData
+		config = open('Logs/harness_log_backup.txt', 'w'); check_dict = Application.configData;
 	else:
-		config = open('Logs/out.txt', 'w')
-		check_dict = Application.sortedData
+		config = open('Logs/out.txt', 'w'); check_dict = Application.sortedData;
 	for key in Application.key_order:
 		if num != Application.count: # Simplify newline!
 			config.write(key + ": " + check_dict[key] + "\n") # Print w/ newline
@@ -178,25 +160,19 @@ def display_ClassConfigs(name,self):
 	if name == "DetailsPage":
 		inputdata = load_configdata(); # Load Data
 		for key in Application.key_order:
-			if i>4:
-				i = 1; j = j+1; # Column limit exceeded, begin new row
-			labelName = tk.Label(self,font = "Helvetica 12",text=key + ":")
-			Application.DetailsPage_labels.append(labelName)
-			labelName.grid(column=i, row=j)
-			fieldName = tk.Entry(self); fieldName.insert(5,inputdata[key]); # Create entry, add data
-			fieldName.grid(column=i+2, row=j); fieldName.configure(state="readonly") 
-			Application.DetailsPage_entries.append(fieldName) # Store Label widgets in a list
+			if i>4:	i = 1; j = j+1; # Column limit exceeded, begin new row
+			labelName = tk.Label(self,font = "Helvetica 12",text=key + ":"); labelName.grid(column=i, row=j);
+			fieldName = tk.Entry(self); fieldName.grid(column=i+2, row=j);
+			fieldName.insert(5,inputdata[key]);	fieldName.configure(state="readonly");
+			Application.DetailsPage_entries.append(fieldName); Application.DetailsPage_labels.append(labelName); # Store widgets 
 			i = i+3 # Column for Second label/entry pair
 	else:
 		for key in Application.key_order:
-			if i>4:
-				i = 1; j = j+1; # Column limit exceeded, begin new row
-			labelName = tk.Label(self,font = "Helvetica 12",text=key + ":")
-			Application.EditPage_labels.append(labelName)
-			labelName.grid(column=i, row=j)
-			fieldName = tk.Entry(self); fieldName.insert(5,Application.configData[key]); # Create entry, add data
-			fieldName.grid(column=i+2, row=j)
-			Application.EditPage_entries.append(fieldName) # Store Entry widgets in a list
+			if i>4: i = 1; j = j+1; # Column limit exceeded, begin new row
+			labelName = tk.Label(self,font = "Helvetica 12",text=key + ":"); labelName.grid(column=i, row=j);
+			fieldName = tk.Entry(self); fieldName.grid(column=i+2, row=j);
+			fieldName.insert(5,Application.configData[key]); # Create entry, add data
+			Application.EditPage_entries.append(fieldName); Application.EditPage_labels.append(labelName); # Store widgets
 			i = i+3 # Column for Second label/entry pair
 		Application.firstEdit = False # Config settings have been edited
 	pad_children(self) # Assign padding to child widgets
@@ -225,7 +201,7 @@ class StatusCheckPage(tk.Frame):
 	def set_page(self, controller):
 		label = tk.Label(self, font = LARGE_FONT, text = "Status Check\n").grid(row=0, column=1,columnspan=3)
 		error = False
-		if len(Application.errorLog)>0: error=True
+		if len(Application.errorLog)>0: error=True;
 		gobackbutton = tk.Button(self, bd = "2", fg = "white", bg = "blue", font = "Helvetica 12", text="Home",command=lambda: window_asktocancel("Home",controller,error)).grid(row=6,column=4,rowspan=1)
 		exitButton = tk.Button(self, bd = "2", fg = "white", bg = "red", font = "Helvetica 12", text ="Close", command=lambda: window_asktocancel("Quit",controller,error)).grid(row=6,column=5,rowspan=1)
 		self.load_progress(controller)
@@ -234,9 +210,8 @@ class StatusCheckPage(tk.Frame):
 		for key in sorted(Application.StatusPage_checks):
 			labelName = tk.Label(self,font = "Helvetica 12",text= key + "  ").grid(column=i, row=j)
 			labelSecond = tk.Label(self, font = "Helvetica 10",text=Application.StatusPage_checks[key], fg = "white")
-			if Application.StatusPage_checks[key] == "PASS":
-				labelSecond.configure(bg = "Green")
-			else:
+			if Application.StatusPage_checks[key] == "PASS": labelSecond.configure(bg = "Green");
+			else: 
 				labelSecond.configure(bg = "Red"); logbutton = tk.Button(self, bd = "2", fg = "white", bg = "gray", font = "Helvetica 9", text ="See Log", command = lambda: window_popup("Error Log",Application.errorLog[0])).grid(row=4,column=4,rowspan=1)
 			labelSecond.grid(column=i+2, row=j)
 			j = j+1 # New row
